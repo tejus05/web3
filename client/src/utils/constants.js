f275554933ea1a3a39e5150c492be2ddfb6ed807
tejus <thequantumcoder@gmail.com>
2024-05-14 21:40:31 +0530

    deploy smart contract on sepolia

---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from './Transactions.json'
+
+export const contractABI = abi.abi;
+export const contractAddress = "0x9B1BC948b5EB31c5F98f6a939793B2F5A60f3Cd1";
--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,12 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomiclabs/hardhat-waffle');
+require('dotenv').config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
+  solidity: '0.8.0',
+  networks: {
+    sepolia: {
+      url: process.env.ALCHEMY_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
